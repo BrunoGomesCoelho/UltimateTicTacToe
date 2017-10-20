2191b2881b310771b6be03bd025b14f3d6b516aa
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-10-20 20:13:51 -0200

    Start working on TicTacBoard class

--- TicTacBoard.py
+++ TicTacBoard.py
@@ -4,10 +4,13 @@
 
 class TicTacBoard:
 	def __init__(self):
-		self.board = None
+		self.board = []*9
 		self.is_full = None
 		self.has_winner = None
 		self.winner = None
 
 	def move(self, piece, num):
-		pass
+		if self.board[num] is not None:
+			raise Exception("Invalid position")
+		else:
+			self.board[num] = piece
