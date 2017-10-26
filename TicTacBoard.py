57036a08956d34c814aabe01f52a6ed0a49a1784
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-10-25 23:41:10 -0200

    Add check_win function

--- TicTacBoard.py
+++ TicTacBoard.py
@@ -9,8 +9,35 @@ class TicTacBoard:
 		self.has_winner = None
 		self.winner = None
 
-	def move(self, piece, num):
-		if self.board[num] is not None:
-			raise Exception("Invalid position")
+	def valid_move(self, move):
+		if move < 0 or move > 8:
+			raise IndexError("Piece out of bounds")
+		if self.board[move] is not None:
+			raise ValueError("Position already filled in")
 		else:
+			return True
+
+	def move(self, piece, num):
+		try:
+			self.valid_move(num)
+		except IndexError or ValueError:
+			print(Exception)
+		if self.valid_move(num):
 			self.board[num] = piece
+			self.check_win(num)
+
+	def check_win(self, piece):
+		row_count = col_count = diag1_count = diag2_count = 0
+
+		for i in range(3):
+			for j in range(3):
+				if self.board[i*3 + j] == piece:    # checks rows
+					row_count += 1
+				if self.board[j + 3 * i] == piece:  # checks cols
+					col_count += 1
+
+		for i in range(3):
+			if self.board[i*4] == piece:    # checks main diagonal
+				diag1_count += 1
+			if self.board[i*2 + 2] == piece:    # checks secondary diagonal
+				diag2_count += 1
