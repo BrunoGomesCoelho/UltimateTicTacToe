ef3fbeaae8ae469513230978aeae56ce1f895304
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-10-26 16:40:20 -0200

    Add valid_pos function tests

--- test_board.py
+++ test_board.py
@@ -1,5 +1,23 @@
 import pytest
 
+from Board import Board
+
 
 class TestBoard(object):
-	pass
+
+	def test_valid_pos_lower_0(self):
+		with pytest.raises(Exception):
+			b = Board()
+			b.valid_pos(-1)
+
+	def test_valid_pos_bigger_8(self):
+		with pytest.raises(Exception):
+			b = Board()
+			b.valid_pos(9)
+
+	def test_valid_pos_valid(self):
+		b = Board()
+		test = True
+		for i in range(9):
+			test = test and b.valid_pos(i)
+		assert test
